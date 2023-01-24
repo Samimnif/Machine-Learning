e14dc066ede329837cbc9834375c2606539f563f
samimnif <samimnif@icloud.com>
2023-01-24 16:15:08 +0000

    Classification code + Figures

--- supervised/classification.py
+++ supervised/classification.py
@@ -1,31 +1,47 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("bank-full.csv")
-#,"volatileacidity","citricacid","residualsugar","chlorides","freesulfurdioxide","totalsulfurdioxide","density","pH","sulphates","alcohol"
-x = data.drop("balance", axis=1)
-y = data["age"]
+data = pd.read_csv("winequality-red.csv")
 
-le = LabelEncoder()
-le.fit(data[""])
+encoder = LabelEncoder()
+data["quality"] = encoder.fit_transform(data["quality"])
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+X = data[["fixedacidity","volatileacidity","citricacid","residualsugar","chlorides","freesulfurdioxide","totalsulfurdioxide","density","pH","sulphates","alcohol"]]
+y = data["quality"]
 
-model = LogisticRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model.fit(X_train,y_train)
+clf = RandomForestClassifier()
 
-y_pred = model.predict(X_test)
+clf.fit(X_train, y_train)
 
-plt.scatter(y_test, y_pred)
-plt.xlabel("true labels")
-plt.ylabel("Predicted labels")
-plt.title("Scatter plot of predicted vs actual labels")
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion matric:\n", conf_matrix)
+
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap="YlGnBu") #, cmap="Y1GnBu"
+plt.xlabel("Predicted")
+plt.ylabel("True")
 plt.show()
 
-sns.heatmap(x.corr(), annot=True)
+plt.scatter(y_test, y_pred)
+plt.xlabel("True Values")
+plt.ylabel("predictions")
+plt.show()
+
+sns.boxplot(data=X)
+plt.show()
+
+sns.pairplot(X, diag_kind='kde')
 plt.show()
