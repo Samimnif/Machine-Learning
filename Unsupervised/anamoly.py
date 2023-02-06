edd3ce3c961799a461e531d8a2850244143d2f41
samimnif <samimnif@icloud.com>
2023-02-06 10:53:12 -0500

    Unsupervised learning

--- Unsupervised/anamoly.py
+++ Unsupervised/anamoly.py
@@ -1,44 +1,52 @@
-import numpy as np
 import pandas as pd
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.mixture import GaussianMixture
-from scipy.stats import multivariate_normal
+import seaborn as sns
+from sklearn.ensemble import IsolationForest
+from scipy import stats
 
-# Load the data
-data = pd.read_csv("data.csv")
+# Load the Daily Delhi Climate Train dataset
+df = pd.read_csv('DailyDelhiClimateTrain.csv')
 
-# Extract the features to be used in the anomaly detection algorithm
-features = data.iloc[:, [2, 3, 4, 5]].values
+# Select the features and target
+X = df.iloc[:, :5].values
 
-# Fit the Gaussian Mixture Model to the data
-gmm = GaussianMixture(n_components = 3)
-gmm.fit(features)
+# Remove outliers using Z-score
+z = np.abs(stats.zscore(X))
+X = X[(z < 3).all(axis=1)]
 
-# Predict the probabilities of each data point belonging to each cluster
-probs = gmm.predict_proba(features)
+# Fit Isolation Forest on the data
+isolation_forest = IsolationForest(contamination=0.1)
+isolation_forest.fit(X)
 
-# Compute the anomaly scores for each data point
-scores = np.max(probs, axis = 1)
+# Predict the anomaly score for each datapoint
+y_pred = isolation_forest.decision_function(X)
+y_pred = pd.Series(y_pred, name='Anomaly Score')
 
-# Plot the Gaussian distributions for each cluster
-for i in range(3):
-    mean = gmm.means_[i,:]
-    cov = gmm.covariances_[i,:]
-    x, y = np.mgrid[mean[0]-2:mean[0]+2:.1, mean[1]-2:mean[1]+2:.1]
-    pos = np.empty(x.shape + (2,))
-    pos[:, :, 0] = x
-    pos[:, :, 1] = y
-    rv = multivariate_normal(mean, cov)
-    plt.contourf(x, y, rv.pdf(pos))
+# Plot the Gaussian Mixture Model (GMM)
+g = sns.jointplot(x=X[:, 0], y=X[:, 1], kind='kde')
 
-# Plot the scatter plot of the features, coloring each data point based on its anomaly score
-plt.scatter(features[:, 0], features[:, 1], c = scores, cmap = 'viridis')
+# Plot the scatter plot of the dataset with different colors for anomalies and inliers
+plt.scatter(X[y_pred > 0, 0], X[y_pred > 0, 1], s=100, c='blue', label='Inliers')
+plt.scatter(X[y_pred < 0, 0], X[y_pred < 0, 1], s=100, c='red', label='Anomalies')
+plt.title('Anomaly detection in Daily Delhi Climate Train data')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend()
+plt.show()
 
-# Plot the heatmap of the features
-sns.heatmap(data.corr(), annot = True)
+# Plot the heatmap of the Daily Delhi Climate Train dataset
+plt.figure(figsize=(12, 6))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.show()
 
-# Plot the boxplot of the features, coloring each data point based on its anomaly score
-sns.boxplot(x = "variable", y = "value", data = pd.melt(data[data.columns[2:6]], value_vars = data.columns[2:6]), hue = "value", hue_norm = (0, 1), palette = "viridis")
+# Plot histograms of the features
+df_features = df.iloc[:, :5]
 
+plt.figure(figsize=(20, 12))
+for i, col in enumerate(df_features.columns):
+    plt.subplot(3, 2, i+1)
+    plt.hist(df_features[col], color='blue', alpha=0.5)
+    plt.title(col)
+plt.tight_layout()
 plt.show()
