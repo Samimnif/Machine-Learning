18ffc0510dba87b36e3cfb4d79cb9845d0c8dd48
samimnif <samimnif@icloud.com>
2023-02-06 08:51:53 -0500

    Unsupervised learning

--- Unsupervised/clustering.py
+++ Unsupervised/clustering.py
@@ -1,45 +1,39 @@
-import numpy as np
 import pandas as pd
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.cluster import KMeans
-from scipy.stats import multivariate_normal
+from sklearn.decomposition import PCA
 
-# Load the data
-data = pd.read_csv("winequality-white.csv")
+# Load the wine quality dataset
+df = pd.read_csv('winequality-white.csv')
 
-# Extract the features and target to be used in the clustering algorithm
-features = data.iloc[:, [0, 1, 2, 3, 4, 5]].values
-target = data.iloc[:, 11].values
+# Select the features and target
+X = df.iloc[:, :6].values
+y = df.iloc[:, -1].values
 
-# Fit the K-Means algorithm to the data
-kmeans = KMeans(n_clusters = 6, init = 'k-means++', random_state = 0)
-y_kmeans = kmeans.fit_predict(features)
+# Perform PCA to visualize the data in 2D
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
 
-# Plot the Gaussian distributions for each cluster
-for i in range(6):
-    mean = kmeans.cluster_centers_[i,:]
-    cov = np.cov(features[y_kmeans == i].T)
-    x, y = np.mgrid[mean[0]-2:mean[0]+2:.1, mean[1]-2:mean[1]+2:.1]
-    pos = np.empty(x.shape + (2,))
-    pos[:, :, 0] = x
-    pos[:, :, 1] = y
-    rv = multivariate_normal(mean, cov)
-    plt.contourf(x, y, rv.pdf(pos))
+# Fit KMeans on the PCA-reduced data
+kmeans = KMeans(n_clusters=3)
+y_kmeans = kmeans.fit_predict(X_pca)
 
-# Plot the heatmap of the features
-sns.heatmap(data.corr(), annot = True)
+# Plot the Gaussian Mixture Model (GMM)
+g = sns.jointplot(x=X_pca[:, 0], y=X_pca[:, 1], kind='kde')
 
-# Plot the data with different colors for each cluster
-plt.scatter(features[y_kmeans == 0, 0], features[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(features[y_kmeans == 1, 0], features[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(features[y_kmeans == 2, 0], features[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-
-# Plot the centroids of each cluster
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
-
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (1-100)')
+# Plot the scatter plot of the clusters with each unique color
+plt.scatter(X_pca[y_kmeans == 0, 0], X_pca[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X_pca[y_kmeans == 1, 0], X_pca[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X_pca[y_kmeans == 2, 0], X_pca[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.title('Clusters of wine quality data')
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
 plt.legend()
+plt.show()
+
+# Plot the heatmap of the wine quality dataset
+plt.figure(figsize=(12, 6))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
 plt.show()
