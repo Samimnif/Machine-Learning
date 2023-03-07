45c189a231ecba3a723f0c782f8fae216d10e005
samimnif <samimnif@icloud.com>
2023-03-07 15:35:19 -0500

    ML Project - Flask Framework - Prediction Model generation

--- ML Final Project/ML_CardioModel.py
+++ ML Final Project/ML_CardioModel.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pickle
 
 # Load the dataset
@@ -23,3 +26,58 @@ print(f"Accuracy: {accuracy}")
 
 # Save the trained model to a pickle file
 pickle.dump(rfc, open("cardio_model.pkl", "wb"))
+
+if __name__ == "__main__":
+    # Plot a histogram of the age distribution
+    plt.hist(df['age'], bins=20)
+    plt.title("Age Distribution")
+    plt.xlabel("Age")
+    plt.ylabel("Count")
+    plt.show()
+
+    # Plot a boxplot of the blood pressure distribution
+    sns.boxplot(x=df['ap_hi'])
+    plt.title("Blood Pressure Distribution")
+    plt.xlabel("Blood Pressure")
+    plt.show()
+
+    # Plot a gaussian kde of the height distribution
+    sns.kdeplot(x=df['height'], fill=True)
+    plt.title("Height Distribution")
+    plt.xlabel("Height")
+    plt.show()
+
+    # Plot a heatmap of the feature correlations
+    corr = df.corr()
+    sns.heatmap(corr, cmap="coolwarm", annot=True)
+    plt.title("Feature Correlation Heatmap")
+    plt.show()
+
+    # Plot the AUC-ROC curve
+    y_pred = rfc.predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.title('Receiver Operating Characteristic Curve')
+    plt.show()
+
+    # Plot a scatter plot of weight and height colored by cardio status
+    sns.scatterplot(x='weight', y='height', hue='cardio', data=df)
+    plt.title("Weight vs Height by Cardio Status")
+    plt.show()
+
+    # Plot pairplot
+    sns.pairplot(data=df, hue='cardio')
+    plt.title("Pairplot")
+    plt.show()
+
+    # Plot barplot of gender
+    sns.countplot(data=df, x='gender', hue='cardio')
+    plt.title("Gender Distribution")
+    plt.show()
--- ML Final Project/ML_DiabetesModel.py
+++ ML Final Project/ML_DiabetesModel.py
@@ -35,10 +35,34 @@ if __name__ == "__main__":
 
     # Plot confusion matrix
     from sklearn.metrics import confusion_matrix
+
     y_pred = rfc.predict(X_test)
     cm = confusion_matrix(y_test, y_pred)
     sns.heatmap(cm, annot=True, cmap="Blues")
     plt.title("Confusion Matrix")
     plt.xlabel("Predicted")
     plt.ylabel("True")
+    plt.show()
+
+    # Plot histogram of BMI values
+    plt.hist(X["BMI"], bins=20)
+    plt.xlabel("BMI")
+    plt.ylabel("Count")
+    plt.title("Histogram of BMI Values")
+    plt.show()
+
+    # Plot boxplot of glucose values by diabetes outcome
+    sns.boxplot(x="Outcome", y="Glucose", data=data)
+    plt.xlabel("Diabetes Outcome")
+    plt.ylabel("Glucose")
+    plt.title("Boxplot of Glucose Values by Diabetes Outcome")
+    plt.show()
+
+    # Plot density plot of age values by diabetes outcome
+    plt.figure(figsize=(8, 5))
+    sns.kdeplot(data.loc[data["Outcome"] == 0, "Age"], shade=True, label="No Diabetes")
+    sns.kdeplot(data.loc[data["Outcome"] == 1, "Age"], shade=True, label="Diabetes")
+    plt.xlabel("Age")
+    plt.ylabel("Density")
+    plt.title("Density Plot of Age Values by Diabetes Outcome")
     plt.show()
